cac13823817de851bf47c115600258540283bcf7
Arnatech <anoldtech64@gmail.com>
2024-09-24 11:41:03 +0000

    user

--- user_authentication/users/views.py
+++ user_authentication/users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from .models import User
+from .serializers import UserRegistrationSerializer, UserLoginSerializer 
 
-# Create your views here.
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully"}, status = status.HTTP_201_CREATED)
+        return Response
+    
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data = request.data)
+        if serializer.is_valid():
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
+
+            try:
+                user = User.objects.get(username = username)
+                if user.check_password(password):
+                    #Generate Token later
+                    return Response({"message": "Login successful"}, status = status.HTTP_200_OK)
+                else:
+                    return Response({"error": "Login successful"}, status = status.HTTP_400_BAD_REQUEST)
+            except User.DoesNotExist:
+                return Response({"error": "User does not exist"},status = status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)            
